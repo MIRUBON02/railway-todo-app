55f37de61ce8649d93ad20ad40da352e8778c604
MikuInagaki <asterism.shootingstar@gmail.com>
2025-07-22 20:38:42 +0900

    React v19対応するよう変更

--- src/components/BackButton.jsx
+++ src/components/BackButton.jsx
@@ -1,11 +1,15 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
 import { ChevronIcon } from '~/icons/ChevronIcon'
 import './BackButton.css'
 
-const handleClick = () => {
-  window.history.back()
-}
-
 export const BackButton = () => {
+  const navigate = useNavigate()
+  const handleClick = () => {
+    navigate(-1)
+  }
+
+
   return (
     <button type="button" onClick={handleClick} className="back_button">
       <ChevronIcon className="back_button__icon" />
